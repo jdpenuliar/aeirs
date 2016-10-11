e4498141bf78d957ba87c3a082bc324e7f4293fb
jdpenuliar <jaydz.penuliar@gmail.com>
2016-10-10 19:22:03 -0700

    add interceptor partially

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,12 +11,4 @@ module.exports = function(app){
 	app.post("/register",function(req,res){
 		users.create(req,res);
 	});
-	// app.get('/protected',
-	// jwt({secret: 'shhhhhhared-secret'}),
-	// function(req, res) {
-	// 	if (!req.user.admin){
-	// 		return res.sendStatus(401);
-	// 	}
-	// 	res.sendStatus(200);
-	// });
 }
