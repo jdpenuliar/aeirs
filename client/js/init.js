7852fc05aa7a7e247232b5487b455447d7f3534f
jdpenuliar <jaydz.penuliar@gmail.com>
2016-10-27 19:51:29 -0700

    datables first try

--- client/js/init.js
+++ client/js/init.js
@@ -1,7 +1,6 @@
 (function($){
 	$(function(){
 		$('.button-collapse').sideNav();
-		$('.parallax').parallax();
 		$(".dropdown-button").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
