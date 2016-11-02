74090d5decac13dbca08aca74e459ab22a00ca93
jdpenuliar <jaydz.penuliar@gmail.com>
2016-11-01 21:53:46 -0700

    fixed comment

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
 // user level
 // 0 - admin: can do everything (just for developers)
-// 1 - principal: can CRUD faculty and ONLY View students and classes that the faculty makes
+// 1 - principal: can CRUD faculty and CRUD student and View classes that the faculty makes
 // 2 - faculty: can CRUD class and CRUD student to be. CLASS will be added later. it will be on a different Schema
 var Schema = new mongoose.Schema({
 	userName: {type: String, required: true, minlength: 1},
