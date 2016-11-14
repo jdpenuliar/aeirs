df0ffe3bd390e3205abc5961751e45574e9b3ffe
shivbhardwaj <sbhardwaj192@gmail.com>
2016-11-13 18:32:00 -0800

    created new for sections.js

--- server/controllers/sections.js
+++ server/controllers/sections.js
@@ -4,6 +4,29 @@ var Section = mongoose.model('Section');
 
 module.exports=(function(){
 	return{
-
+		new_section: function(req, res){
+			var new_section = new Section(req.body);
+			GradeLevel.findOne({_id: req.params._id}, function(err, gradeLevelFromDb){
+				if(err){
+					res.send('this is an err', err);
+				} else{
+					new_section._grade_level=gradeLevelFromDb._id;
+					gradeLevelFromDb.sections.push(new_section);
+					gradeLevelFromDb.save(function(err, result){
+						if(err){
+							res.send('err saving the gradeLevelFromDb', err);
+						} else{
+							new_section.save(function(err, result){
+								if(err){
+									res.send('err saving new_section', err);
+								} else{
+									res.json(result);
+								}
+							})
+						}
+					})
+				}
+			})
+		}
 	}
 })();
