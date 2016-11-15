408319aea5992d96bdb722824e404b281067fb0c
jdpenuliar <jaydz.penuliar@gmail.com>
2016-11-15 07:39:47 -0800

    remove comment

--- server/controllers/classes.js
+++ server/controllers/classes.js
@@ -65,22 +65,6 @@ module.exports=(function(){
 									}
 								});
 
-
-                                // Product.find({}).populate("productBids").exec(function(err,data){
-                                //     if(err){
-                                //         res.json(err);
-                                //     }else{
-                                //         Product.populate(data,{path: "productBids.bider", model:"User"},function(err,data){
-                                //             if(err){
-                                //                 res.json(err);
-                                //             }else{
-                                //                 console.log(data);
-                                //                 res.json(data);
-                                //             }
-                                //         });
-                                //     }
-                                // });
-
                             }
                         });
 						
