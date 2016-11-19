a9372d34e3e36289192af3da3e58aa82f96072e6
shivbhardwaj <sbhardwaj192@gmail.com>
2016-11-19 11:04:39 -0800

    added cookies to show_section controller

--- client/controllers/showSectionController.js
+++ client/controllers/showSectionController.js
@@ -2,14 +2,21 @@ AEIRSAppModule.controller('showSectionController', function($scope, $routeParams
 
 	console.log('I am able to load my showSectionController along with my show_section partial');
 
-	//dont wrap in scope, because we want this to show as soon as page loads
+	var logged_in_user = $cookies.get('logged_user');
+	$scope.firstName = $cookies.get("firstName");
+	$scope.userLevel = $cookies.get("userLevel");
+	$scope.lastName = $cookies.get("lastName");
+	$scope.emailAddress = $cookies.get("emailAddress");
+	console.log('this is the cookie data',  $scope.firstName )
+	if(!logged_in_user){
+		$location.url('/')
+	}
 
-	// console.log('this is routeParams ------\n ', $routeParams);
-	// var GL_ID=$routeParams.id;
-	// sectionFactory.getSections(GL_ID, function(data){
-	// 	console.log('this is data in allSectionsController getSections', data);
-	// 	$scope.sections=data;
-	// })
+	//log out method
+	$scope.logout = function(){
+		$cookies.remove('logged_user');
+		$location.url('/');
+	}
 
 	var sectionID=$routeParams.id;
 	console.log('this is routeParams.id ', $routeParams.id, "this is sectionID", sectionID);
