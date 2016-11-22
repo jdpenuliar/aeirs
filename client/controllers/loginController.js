22f60d0efaff1699ca4d099f2891b0818c5f1e9e
shivbhardwaj <sbhardwaj192@gmail.com>
2016-11-21 22:03:52 -0800

    removed unnecessary a tag

--- client/controllers/loginController.js
+++ client/controllers/loginController.js
@@ -24,6 +24,7 @@ angular.module('AEIRS').controller('loginController', function($scope, $location
     })
   }
 
+  // var userID=$routeParams.id;
   // userFactory.getSection(userID, function(data){
   //   console.log('this is data from show controller-------------\n', data);
   //   $scope.user=data.data;
